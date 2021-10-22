71c8965ed1fc8b650e9f1f77b6215e9e3503dd3b
gel1123 <atno.14@gmail.com>
2021-10-22 10:15:54 +0900

    add

--- statics/SigDayjs.mjs
+++ statics/SigDayjs.mjs
@@ -9,12 +9,12 @@
 // console.log('mdc2', mdc2.getDate(), mdc2);
 
 class SigDayjs {
-    constructor() {
-        this.init();
+    constructor(date) {
+        this.init(date);
     }
-    init(dateObj) {
+    init(date) {
         this.$L = undefined;
-        this.$d = dateObj && new Date(dateObj) || new Date();
+        this.$d = date && new Date(date) || new Date();
         this.$x = undefined;
         this.$y = this.$d.getFullYear();
         this.$M = this.$d.getMonth();
@@ -28,8 +28,14 @@ class SigDayjs {
         // 実行成功
         return true;
     }
-    year() {
-        return this.$y;
+    clone() {
+        return new SigDayjs(this.$d);
+    }
+    toDate() {
+        return this.$d;
+    }
+    year(yearNum) {
+        return yearNum && this.init(this.$d.setFullYear(yearNum)) && this || this.$y;
     }
     month(monthNum) {
         return monthNum && this.init(this.$d.setMonth(monthNum)) && this || this.$M;
@@ -40,17 +46,17 @@ class SigDayjs {
     day() {
         return this.$W;
     }
-    hour() {
-        return this.$H;
+    hour(hourNum) {
+        return hourNum && this.init(this.$d.setHours(hourNum)) && this || this.$H;
     }
-    minutes() {
-        return this.$m;
+    minutes(minNum) {
+        return minNum && this.init(this.$d.setMinutes(minNum)) && this || this.$m;
     }
-    second() {
-        return this.$s;
+    second(secNum) {
+        return secNum && this.init(this.$d.setSeconds(secNum)) && this || this.$s;
     }
-    millisecond() {
-        return this.$ms;
+    millisecond(msNum) {
+        return msNum && this.init(this.$d.setMilliseconds(msNum)) && this || this.$ms;
     }
     format(formatStr) {
         /* 対応済みフォーマット（例：2021年9月1日水曜なら）
@@ -86,10 +92,94 @@ class SigDayjs {
         });
         return formatStr;
     }
+    add(num, unit) {
+        // Number(num)と同じく、numが数値であることを保証する（undefinedや文字列ならNaN）
+        num = num - 0;  // <= マイナスゼロなら実際の値に影響はないが、ここでもし誤って「マイナス1」などにしてしまったら当然のことながらNG（うっかりしてるとやりかねないので気をつけるべき...）
+
+        // ES6で動作させる前提
+        if (Number.isNaN(num)) {
+            throw new Error("first argument is NaN");
+        }
+
+        /* 対応するunit（単位）
+         * day	       d  <= Day
+         * week        w  <= Week
+         * month       M  <= Month
+         * quarter     Q  <= Quarter
+         * year	       y  <= Year
+         * hour	       h  <= Hour
+         * minute      m  <= Minute
+         * second      s  <= Second
+         * millisecond ms <= Millisecond
+         */
+        const convertToShorthand = unit => {
+            // 長いunitから短いunit（ショートハンド）へと変換する（逆だとNG）
+            const alias = {
+                day: "d",
+                week: "w",
+                month: "M",
+                quarter: "Q",
+                year: "y",
+                hour: "h",
+                minute: "m",
+                second: "s",
+                millisecond: "ms"
+            }
+            // formatのような「部分変換」ではなく「完全変換」なので、
+            // millisecondとsecondの誤変換を気にしてキー文字列長降順ソートする必要はない
+            unit = Object.keys(alias).reduce((previous, current) => {
+                // unitとキーが一致しないなら、reduceとして実行しているのでもう一度unitを返却しないとバグが生じる
+                return previous === current && alias[current] || previous;
+            }, unit);
+            // ここが関数定義内であることを忘れないように...（製造中に忘れていたことに気付かなかったため覚書）
+            return unit;
+        };
+        unit = convertToShorthand(unit);
+
+        // 増分msを算出し、addしたdayjsオブジェクトを返却する関数をunitごとに定義 ※immutable
+        const addFunc = {
+            /**
+             * *****************
+             * ***** 注意 ******
+             * *****************
+             * 
+             * 下記はコードとしてNG。
+             * ```
+             * new Date() + num
+             * ```
+             * 
+             * なぜなら、DateオブジェクトはNumberではないから。
+             * DateオブジェクトをNumberにしたいなら、下記の方法を使うべき
+             * ```
+             * // 方法1（JS仕様による減算時の暗黙のNumber変換を活用）
+             * new Date()-0 + num // <= マイナスゼロなら実際の値に影響はないが、ここでもし誤って「マイナス1」などにしてしまったら当然のことながらNG（うっかりしてるとやりかねないので気をつけるべき...）
+             * 
+             * // 方法2
+             * new Date().getTime()
+             * ```
+             * 
+             */
+            "d": num => new SigDayjs(this.$d - 0 + num * 1000 * 60 * 60 * 24),
+            "w": num => new SigDayjs(this.$d - 0 + num * 1000 * 60 * 60 * 24 * 7),
+            "M": num => new SigDayjs(this.$d - 0 + new Date(this.$d).setMonth(this.$d.getMonth() + num) - new Date(this.$d)),
+            "Q": num => new SigDayjs(this.$d - 0 + new Date(this.$d).setMonth(this.$d.getMonth() + num * 3) - new Date(this.$d)),
+            "y": num => new SigDayjs(this.$d - 0 + new Date(this.$d).setFullYear(this.$d.getFullYear() + num) - new Date(this.$d)),
+            "h": num => new SigDayjs(this.$d - 0 + num * 1000 * 60 * 60),
+            "m": num => new SigDayjs(this.$d - 0 + num * 1000 * 60),
+            "s": num => new SigDayjs(this.$d - 0 + num * 1000),
+            "ms": num => new SigDayjs(this.$d - 0 + num)
+        }
+        return addFunc[unit](num);
+    }
 }
 
-const sigDayjs = () => {
-    return new SigDayjs();
+/**
+ * 上記クラスのコンストラクタをコールする導線なので、
+ * こちらの関数にもコンストラクタ同様の引数を設定しないとNG.
+ * 忘れないように注意（うっかり忘れて時間を費やした実績あり）
+ */
+const sigDayjs = (date) => {
+    return new SigDayjs(date);
 }
 
 export default sigDayjs;
--- statics/SigDayjs.test.mjs
+++ statics/SigDayjs.test.mjs
@@ -1,22 +1,54 @@
 'use strict';
 
 import origindayjs from 'dayjs';
+import quarterOfYear from 'dayjs/plugin/quarterOfYear.js';
+origindayjs.extend(quarterOfYear);
+
 import sigDayjs from './SigDayjs.mjs';
 
-const useDayjsCode = dayjs => {
+const writeLog = result => {
+    console.log(result);
+    console.log('# typeof r        : ', typeof result);
+    console.log('# toString.call(r): ', toString.call(result));
+};
+const useFormat = dayjs => {
     let anyday = dayjs();
     anyday = anyday.month(anyday.month() - 100);
     anyday = anyday.date(anyday.date() - 20);
-    
+
     const r = [anyday.format('YYYY/MM/DD(dd)'), anyday.format('YY-M-D(d)')];
-    console.log(r);
-    console.log('# typeof r        : ', typeof r);
-    console.log('# toString.call(r): ', toString.call(r));
-    return r.toString();
+    writeLog(r);
+    return JSON.stringify(r);
 };
+const useAdd = (dayjs, anyday) => {
+    console.log('before_add: ', anyday);
+    const r = {
+        'd': dayjs(anyday).add(1, 'd').toDate(),
+        'w': dayjs(anyday).add(1, 'w').toDate(),
+        'M': dayjs(anyday).add(1, 'M').toDate(),
+        'Q': dayjs(anyday).add(1, 'Q').toDate(),
+        'y': dayjs(anyday).add(1, 'y').toDate(),
+        'h': dayjs(anyday).add(1, 'h').toDate(),
+        'm': dayjs(anyday).add(1, 'm').toDate(),
+        's': dayjs(anyday).add(1, 's').toDate(),
+        'ms': dayjs(anyday).add(1, 'ms').toDate(),
+    }
+    writeLog(r);
+    return JSON.stringify(r);
+}
+const useDiff = dayjs => {
+
+}
 const assert = (original, sig) => {
-    const resultOriginal = useDayjsCode(original);
-    const resultSig = useDayjsCode(sig);
-    console.assert(resultOriginal === resultSig, "オリジナルと異なる出力がされました");
-}; 
+    const resultOriginalFormat = useFormat(original);
+    const resultSigFormat = useFormat(sig);
+    console.assert(resultOriginalFormat === resultSigFormat, "オリジナルと異なる出力がされました");
+    console.log('------------------------------------------------------------');
+    
+    const anyday = new Date();
+    const resultOriginalAdd = useAdd(original, anyday);
+    const resultSigAdd = useAdd(sig, anyday);
+    console.assert(resultOriginalAdd === resultSigAdd, "オリジナルと異なる出力がされました");
+    console.log('------------------------------------------------------------');
+};
 assert(origindayjs, sigDayjs);
