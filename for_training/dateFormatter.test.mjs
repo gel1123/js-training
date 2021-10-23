60064f9548865394c24154249a73e4224fcf18eb
gel1123 <atno.14@gmail.com>
2021-10-24 00:25:05 +0900

    fix test of dateFormatter

--- for_training/dateFormatter.test.mjs
+++ for_training/dateFormatter.test.mjs
@@ -1,3 +1,6 @@
 import DateFormatter from './dateFormatter.mjs';
 
-console.log(new DateFormatter('2020-1-2').format("YYYY年MM月DD日（dd）"));
+console.assert(
+    new DateFormatter('2020-1-2').format("YYYY年MM月DD日（dd）") === "2020年01月02日（木）", 
+    "[dateFormatter.test.mjs] 期待値と異なります"
+);
