d54f609b034817ad8fd77dc06cd99aeeddf3e6fb
gel1123 <atno.14@gmail.com>
2021-10-25 22:39:02 +0900

    fix flatNestObj

--- for_training/flatNestObj.mjs
+++ for_training/flatNestObj.mjs
@@ -1,14 +1,14 @@
 import { Utils } from "./common_utils.mjs";
 
-var flatNestObj = function(obj, result) {
+var flatNestObj = function (obj, result) {
     var result = result || {};
-    Object.keys(obj).forEach(function(key) {
+    Object.keys(obj).forEach(function (key) {
         if (Utils.isMap(obj[key])) {
             flatNestObj(obj[key], result)
         } else {
             result[key] = obj[key];
         }
-    }, result);
+    });
     return result;
 };
 
